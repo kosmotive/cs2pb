6a3d9bb404e59bd88e15f647a28b75ca89f0476b
Leonid Kostrykin <void@evoid.de>
2024-07-24 22:18:12 +0200

    Upgrade to latest bootstrapping

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export REF="13e28581333d883d682a65ff84132b70d46a822a"
+export REF="eeb746f1642aa46cee01415c19ed5d700337f0f4"
 
 git clone git@github.com:kodikit/cs2pb-bootstrap.git
 cd cs2pb-bootstrap
