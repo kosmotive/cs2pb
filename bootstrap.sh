7c4353da57c9bd6d2f88e2cf700db1ae9105f48f
Leonid Kostrykin <kostrykin@evoid.de>
2024-07-28 16:30:08 +0200

    Upgrade to newest bootstrap routine

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export REF="eeb746f1642aa46cee01415c19ed5d700337f0f4"
+export REF="65ea3446c0b8d1397e125d94008ecb90e6aaf544"
 
 git clone git@github.com:kodikit/cs2pb-bootstrap.git
 cd cs2pb-bootstrap
