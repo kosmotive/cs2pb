8dfe3cee4f9a0513dbb89971c8b4a8682ec84c8f
Leonid Kostrykin <void@evoid.de>
2024-07-29 00:28:41 +0200

    Fix bug

--- django/url_forward/__init__.py
+++ django/url_forward/__init__.py
@@ -5,5 +5,5 @@ from django.urls import reverse
 
 def get_redirect_url_to(url):
     return reverse('redirect', kwargs = dict(
-        encoded_url = base64.b64decode(url).decode('utf-8')
+        encoded_url = base64.b64encode(url_bytes.encode('utf-8')).decode('utf-8')
     ))
--- django/url_forward/views.py
+++ django/url_forward/views.py
@@ -4,6 +4,6 @@ from django.shortcuts import render
 
 
 def do_redirect(request, encoded_url):
-    url = base64.b64encode(encoded_url.encode(encoding='utf-8'))
+    url = base64.b64decode(encoded_url.encode('utf-8')).decode('utf-8')
     context = dict(url = url)
     return render(request, 'url_forward/redirect.html', context)
