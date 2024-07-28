1756fccb5dea00973fab81b87b79010cd04f86c9
Leonid Kostrykin <void@evoid.de>
2024-07-29 00:49:25 +0200

    Fix typo

--- django/accounts/models.py
+++ django/accounts/models.py
@@ -229,7 +229,7 @@ class Squad(models.Model):
             if account is not None:
                 yield account
 
-    def do_changelog_announcements(self, base_url=None, changelog=None):
+    def do_changelog_announcements(self, base_url='', changelog=None):
         if changelog is None:
             from gitinfo import changelog
 
@@ -247,7 +247,7 @@ class Squad(models.Model):
             if len(announcements) > 0:
                 obscure_url = lambda url: base_url + get_redirect_url_to(url)
                 fmt = lambda entry: f'\n\n🚀 **{entry["date"]}:** {entry["message"]} [More info]({obscure_url(entry["url"])})'
-                text = 'I have just received some updats:' + ''.join(fmt(entry) for entry in announcements)
+                text = 'I have just received some updates:' + ''.join(fmt(entry) for entry in announcements)
 
                 from discordbot.models import ScheduledNotification
                 ScheduledNotification.objects.create(squad = self, text = text)
