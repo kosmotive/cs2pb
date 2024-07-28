8f5118ef4cd90b4549c4a67a0e370bfecdd4b62c
Leonid Kostrykin <void@evoid.de>
2024-07-29 00:22:15 +0200

    Switch to base64 encoding

--- django/url_forward/__init__.py
+++ django/url_forward/__init__.py
@@ -1,9 +1,9 @@
-import urllib
+import base64
 
 from django.urls import reverse
 
 
 def get_redirect_url_to(url):
     return reverse('redirect', kwargs = dict(
-        encoded_url = urllib.parse.quote_plus(url)
+        encoded_url = base64.b64decode(url).decode('utf-8')
     ))
--- django/url_forward/views.py
+++ django/url_forward/views.py
@@ -1,9 +1,9 @@
-import urllib
+import base64
 
 from django.shortcuts import render
 
 
 def do_redirect(request, encoded_url):
-    url = urllib.parse.unquote_plus(encoded_url)
+    url = base64.b64encode(encoded_url.encode(encoding='utf-8'))
     context = dict(url = url)
     return render(request, 'url_forward/redirect.html', context)
