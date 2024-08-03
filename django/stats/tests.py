1a12180b3186dbf1707ff593115e4d9a2598a704
Leonid Kostrykin <kostrykin@evoid.de>
2024-08-03 19:04:44 +0200

    Add tests for carrier badge and peach price

--- django/stats/tests.py
+++ django/stats/tests.py
@@ -249,6 +249,40 @@ class MatchBadge__award(TestCase):
         self.assertEqual(badge.participation.pk, mp1.pk)
         self.assertEqual(badge.frequency, 2)
 
+    def test_carrier_badge(self):
+        pmatch = Match__create_from_data().test()
+        mp1 = pmatch.get_participation('76561197967680028')
+        mp2 = pmatch.get_participation('76561197961345487')
+
+        # Test with ADR right below the threshold
+        mp1.adr = 1.99 * mp2.adr
+        mp1.save()
+        models.MatchBadge.award(mp1, list())
+        self.assertEqual(len(models.MatchBadge.objects.filter(badge_type = 'carrier', participation = mp1)), 0)
+
+        # Test with ADR right above the threshold
+        mp1.adr = 2.01 * mp2.adr
+        mp1.save()
+        models.MatchBadge.award(mp1, list())
+        self.assertEqual(len(models.MatchBadge.objects.filter(badge_type = 'carrier', participation = mp1)), 1)
+
+    def test_peach_price(self):
+        pmatch = Match__create_from_data().test()
+        mp4 = pmatch.get_participation('76561198067716219')
+        mp5 = pmatch.get_participation('76561197962477966')
+
+        # Test with ADR right below the threshold
+        mp5.adr = 0.67 * mp4.adr
+        mp5.save()
+        models.MatchBadge.award(mp5, list())
+        self.assertEqual(len(models.MatchBadge.objects.filter(badge_type = 'peach', participation = mp5)), 0)
+
+        # Test with ADR right above the threshold
+        mp5.adr = 0.65 * mp4.adr
+        mp5.save()
+        models.MatchBadge.award(mp5, list())
+        self.assertEqual(len(models.MatchBadge.objects.filter(badge_type = 'peach', participation = mp5)), 1)
+
 
 class Squad__do_changelog_announcements(TestCase):
 
