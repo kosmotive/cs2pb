f14261abf662fcc036e817c960527a66d0a639b7
Leonid Kostrykin <kostrykin@evoid.de>
2024-08-04 18:53:01 +0200

    Bump awpy to 2.0.0b4 and add test

--- django/tests/test_api.py
+++ django/tests/test_api.py
@@ -50,6 +50,43 @@ class fetch_match_details(unittest.TestCase):
         self.assertEqual(round(pmatch['adr']['76561198140806020'], 1), 98.9)
         self.assertEqual(round(pmatch['adr']['76561198064174518'], 1), 63.6)
 
+    def test_awpy_003698946311295336822_1609103086(self):
+        # This test failed with awpy older than 2.0.0.b4 and should pass with newer versions
+        #
+        # TODO: Recover which sharecode corresponds to the demo 003698946311295336822_1609103086.dem.bz2, candidates:
+        # - CSGO-LG9nE-5xkoA-usHfT-oo9Bv-rfdZH
+        # - CSGO-ciZx5-h7tmh-NtRkN-jzWcb-B6LHA
+        # - CSGO-aKe8R-YPeR3-vjBdp-oBGxh-Z5O3A
+        # - CSGO-4P4Gk-u4nGA-XDf7Y-PyKop-mNcqA
+        # - CSGO-8QGsi-sqQH5-MhbXW-xopM2-KquaL
+        #
+        # TODO: Update the data below so that it corresponds to the correct sharecode (and team_scores, and steam_ids).
+        #
+        pmatch = {
+            'sharecode': 'CSGO-a622L-DjJDC-5zwn4-Gx2tf-YYmQD',
+            'timestamp': 1720469310,
+            'summary': SimpleNamespace(
+                map = testsuite.get_demo_path('003698946311295336822_1609103086'),
+                team_scores = (4, 13),
+            ),
+            'steam_ids': [
+                76561197967680028,
+                76561197961345487,
+                76561197961748270,
+                76561198067716219,
+                76561197962477966,
+                76561198298259382,
+                76561199034015511,
+                76561198309743637,
+                76561198140806020,
+                76561198064174518,
+            ],
+        }
+
+        details = api.fetch_match_details(pmatch)
+
+        # TODO: add some checks
+
 
 class api_csgo(unittest.TestCase):
 
