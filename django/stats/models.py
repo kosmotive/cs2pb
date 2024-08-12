kosmotive/cs2pb

commit 90a1a79c20d5f8522fcc515fa5b64c201b0b04f4

Update models.py

django/stats/models.py
@@ -76,7 +76,7 @@ class GamingSession(models.Model):
         ).distinct().order_by('timestamp').annotate(
             result = F('matchparticipation__result')
         ):
-            text += f'\n- *{pmatch.map_name}*, **{pmatch.score_team1}:{pmatch.score_team2}** '
+            text += f'\n- *{pmatch.map_name}*, **{pmatch.score_team1}:{pmatch.score_team2}**, '
             text += dict(
                 w = 'won 🤘',
                 l = 'lost 💩',

commit e4cda8a0d5ce762ac5288ed9b38120988791d5e7

Fix bug introduced in 90a1a79c20d5f8522fcc515fa5b64c201b0b04f4

django/stats/models.py
@@ -76,12 +76,12 @@ class GamingSession(models.Model):
         ).distinct().order_by('timestamp').annotate(
             result = F('matchparticipation__result')
         ):
-            text += f'\n- *{pmatch.map_name}*, **{pmatch.score_team1}:{pmatch.score_team2}**, '
-            text += dict(
-                w = 'won 🤘',
-                l = 'lost 💩',
-                t = 'ended in a draw 🥵',
-            )[pmatch.result]
+            text += f'\n- *{pmatch.map_name}*, **{pmatch.score_team1}:{pmatch.score_team2}**'
+            if pmatch.result in 'wl':
+                text += ' ' + dict(
+                    w = 'won 🤘',
+                    l = 'lost 💩',
+                )[pmatch.result]
         ScheduledNotification.objects.create(squad = self.squad, text = text)
 
         # Determine the rising star

django/stats/tests.py
@@ -981,7 +981,7 @@ class GamingSession__close(TestCase):
         self.assertEqual(
             notification.text,
             'Matches played in this session:\n'
-            '- *de_anubis*, **12:12** ended in a draw 🥵\n'
+            '- *de_anubis*, **12:12**\n'
             '- *de_inferno*, **13:12** won 🤘\n'
             '- *de_dust2*, **12:13** lost 💩',
         )