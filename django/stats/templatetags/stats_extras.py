e4532f99222f7cd224b02c3dc1b86fffaad02064
Leonid Kostrykin <void@evoid.de>
2024-09-10 18:58:20 +0200

    Replace MVPs by PV in match list

--- django/stats/templatetags/stats_extras.py
+++ django/stats/templatetags/stats_extras.py
@@ -1,3 +1,5 @@
+import math
+
 from django import template
 from django.template.defaultfilters import stringfilter
 
@@ -20,6 +22,11 @@ def divide(a, b):
 @register.filter
 def multiply(a, b):
     return a * b
+
+
+@register.filter
+def player_value(match_participation):
+    return math.sqrt(match_participation.kills / match_participation.deaths) * match_participation.adr / 100
 
 
 @register.filter
