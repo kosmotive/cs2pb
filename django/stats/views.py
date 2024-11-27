kosmotive/cs2pb

commit a6d85070ddf12b3a919d36b7622991b93b9bba86

Fix bug when there is not squad buddy performance to report

django/stats/views.py
@@ -116,9 +116,12 @@ def compute_card(
     ):
 
     # Compute the best/worst squad buddy
-    buddy_performances = squad_membership.squad_buddy_performances
-    best_buddy  = max(buddy_performances, key = buddy_performances.get)
-    worst_buddy = min(buddy_performances, key = buddy_performances.get)
+    if buddy_performances := squad_membership.squad_buddy_performances:
+        best_buddy  = max(buddy_performances, key = buddy_performances.get)
+        worst_buddy = min(buddy_performances, key = buddy_performances.get)
+    else:
+        best_buddy  = None
+        worst_buddy = None
 
     def stat(feature):
         value = squad_membership.stats.get(feature.slug, None)
@@ -165,11 +168,13 @@ def compute_card(
         'stats': stats,
         'stats_dict': {s['name']: s['value'] for s in stats},
         'badges': badges,
-        'best_buddy': best_buddy,
-        'worst_buddy': worst_buddy,
-        'best_buddy_performance_increase': 100 * (buddy_performances[best_buddy] - 1),
-        'worst_buddy_performance_decrease': 100 * (1 - buddy_performances[worst_buddy]),
     }
+    if best_buddy:
+        card_data['best_buddy'] = best_buddy
+        card_data['best_buddy_performance_increase'] = 100 * (buddy_performances[best_buddy] - 1)
+    if worst_buddy:
+        card_data['worst_buddy'] = best_buddy
+        card_data['worst_buddy_performance_decrease'] = 100 * (1 - buddy_performances[worst_buddy])
     if getattr(squad_membership.player, 'account', None) is None and squad_membership.squad is not None:
         card_data['invite_url'] = reverse(
             'invite',

django/stats/templates/stats/player.html
@@ -95,6 +95,7 @@ const matchDateList = [ {% for mp in participations %}
 
             <div id="pv-plot" style="width:600px; height: 280px;"></div>
 
+            {% if squad_buddy_performances %}
             <h2>Performance with squad buddies</h2>
             
             <div id="squad-buddies-plot">
@@ -136,6 +137,7 @@ const matchDateList = [ {% for mp in participations %}
                 </div>
 
             </div>
+            {% endif %}
 
             {% if badges.potw %}
 

commit 03f46ac26331382fb2a1b5f85870c5e37e3e0dbc

Fix bug introduced in a6d85070ddf12b3a919d36b7622991b93b9bba86

django/stats/views.py
@@ -173,7 +173,7 @@ def compute_card(
         card_data['best_buddy'] = best_buddy
         card_data['best_buddy_performance_increase'] = 100 * (buddy_performances[best_buddy] - 1)
     if worst_buddy:
-        card_data['worst_buddy'] = best_buddy
+        card_data['worst_buddy'] = worst_buddy
         card_data['worst_buddy_performance_decrease'] = 100 * (1 - buddy_performances[worst_buddy])
     if getattr(squad_membership.player, 'account', None) is None and squad_membership.squad is not None:
         card_data['invite_url'] = reverse(